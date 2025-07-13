d11b05fef9e8fb9166ace5772abe59ddfd75da60
Mert Kirman <mertkirman@macbookair.local>
2025-07-14 01:51:53 +0300

    Add CORS middleware

--- app/main.py
+++ app/main.py
@@ -1,11 +1,24 @@
 from fastapi import FastAPI
 from app.db import create_tables
-from app.models import User, Todo  # Import models so SQLAlchemy registers them
 from app.api import todos, users
+from fastapi.middleware.cors import CORSMiddleware
 
 create_tables()
 
 app = FastAPI()
+
+# CORS middleware to allow cross-origin requests
+allowed_origins = [
+    "http://localhost:3000"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=allowed_origins,
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all HTTP methods
+    allow_headers=["*"],  # Allows all headers
+)
 
 app.include_router(users.router)
 app.include_router(todos.router)
